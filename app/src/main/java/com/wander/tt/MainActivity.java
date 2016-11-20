445377a4cc990f3840185c9018b59eac9d11fba0
wander0515 <805677461@qq.com>
2016-11-21 00:48:07 +0800

    anim

--- app/src/main/java/com/wander/tt/MainActivity.java
+++ app/src/main/java/com/wander/tt/MainActivity.java
@@ -1,7 +1,16 @@
 package com.wander.tt;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.LayoutTransition;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LayoutAnimationController;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,7 +20,6 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.wander.tt.test.Main2Activity;
 import com.wander.tt.test.PermissionActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("native-lib");
     }
+
+    private TextView tv;
+    private ImageView imageView;
+    private LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +48,51 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+//                                startActivity(new Intent(MainActivity.this, PermissionActivity.class));
+                                tv.setVisibility(View.GONE);
                             }
                         }).show();
             }
         });
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv = (TextView) findViewById(R.id.sample_text);
+        imageView = (ImageView) findViewById(R.id.image);
         tv.setText(stringFromJNI());
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setVisibility(View.VISIBLE);
+            }
+        });
+        final TextView textView  = new TextView(this);
+        textView.setText(stringFromJNI());
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                layout.addView(textView);
+                tv.setVisibility(View.VISIBLE);
+            }
+        });
+
+        layout = (LinearLayout) findViewById(R.id.anim_layout);
+        LayoutTransition layoutTransition = new LayoutTransition();
+
+
+        ObjectAnimator animator = new ObjectAnimator();
+        animator = animator.ofFloat(null,"alpha",0f,1.0f,1f).setDuration(5000);
+//        animator.ofFloat(null, "rotationY", 0f, 360f,0f);
+
+        animator.setDuration(5000);
+        layoutTransition.setAnimator(LayoutTransition.APPEARING,animator);
+
+        ObjectAnimator animator2 = new ObjectAnimator();
+        animator2 = animator2.ofFloat(null,"alpha",1.0f,0f,1f).setDuration(5000);
+        animator2.setDuration(5000);
+        layoutTransition.setAnimator(LayoutTransition.DISAPPEARING,animator2);
+
+        layoutTransition.setDuration(5000);
+        layout.setLayoutTransition(layoutTransition);
     }
 
     @Override
